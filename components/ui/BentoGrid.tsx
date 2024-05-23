13361f5c04b52ee58a4c58639e540412c107d4b7
Harshsharma008 <harshsharma01012002@gmail.com>
2024-05-23 13:47:49 +0530

    ui components front page

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -23,9 +23,11 @@ export const BentoGridItem = ({
     className,
     title,
     description,
-    header,
-    icon,
     id,
+    img,
+    imgClassName,
+    spareImg,
+    titleClassName,
 }: {
     className?: string;
     title?: string | React.ReactNode;
@@ -33,6 +35,10 @@ export const BentoGridItem = ({
     header?: React.ReactNode;
     icon?: React.ReactNode;
     id?: number,
+    img?: string;
+    imgClassName?: string;
+    spareImg?: string;
+    titleClassName?: string;
 }) => {
     return (
         <div
@@ -40,10 +46,17 @@ export const BentoGridItem = ({
                 "row-span-1 rounded-xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4",
                 className
             )}
+            style={{
+                //   add these two
+                //   you can generate the color from here https://cssgradient.io/
+                background: "rgb(4,7,29)",
+                backgroundColor:
+                    "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
+            }}
         >
-            {header}
+
             <div className="group-hover/bento:translate-x-2 transition duration-200">
-                {icon}
+
                 <div className="font-sans font-bold text-neutral-600 dark:text-neutral-200 mb-2 mt-2">
                     {title}
                 </div>
